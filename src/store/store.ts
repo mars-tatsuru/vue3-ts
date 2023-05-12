1f00d48f6892b4c34c1076e3384431ef378539ee
tatsuru koike <t-koike@marsflag.com>
2023-05-12 17:38:40 +0900

    feature: 色々

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,7 @@
-import type { InjectionKey } from 'vue';
+import type { InjectionKey } from "vue";
 import { createStore, Store, useStore as baseUseStore } from "vuex";
 import * as MutationTypes from "./mutationType";
 import * as ActionTypes from "./actinonTypes";
-
 
 type TodoItem = {
   id: number;
@@ -23,20 +22,20 @@ export const key: InjectionKey<Store<State>> = Symbol();
 export const store = createStore<State>({
   state: {
     todoItems: [
-     {
+      {
         id: 1,
-       title: "foo",
-       content: "bar",
-       completed: false
-     },
-     {
+        title: "foo",
+        content: "bar",
+        completed: false,
+      },
+      {
         id: 2,
-       title: "head",
-       content: "ver",
-       completed: true
-     },
-   ]
- },
+        title: "head",
+        content: "ver",
+        completed: true,
+      },
+    ],
+  },
   mutations: {
     //関数を定義している
     [MutationTypes.ADD_TODO_ITEM](state, todoItem: TodoItem) {
@@ -44,7 +43,7 @@ export const store = createStore<State>({
     },
     [MutationTypes.INITIALIZE_TODO_ITEMS](store, todoItems: TodoItem[]) {
       store.todoItems = todoItems;
-    }
+    },
   },
   // actions: {
   //   async [ActionTypes.INITIALIZE_TODO_ITEMS]({ commit }) {
@@ -53,13 +52,13 @@ export const store = createStore<State>({
   //   }
   // }
   getters: {
-    completedTodoItems: store => {
-      return store.todoItems.filter(todo => todo.completed);
-    }
-  }
+    completedTodoItems: (store) => {
+      return store.todoItems.filter((todo) => todo.completed);
+    },
+  },
 });
 
 // useStoreを使う時にキーの指定を省略するためのラッパー関数
 export const useStore = () => {
   return baseUseStore(key);
-}
+};
--- src/store/vuex.d.ts
+++ src/store/vuex.d.ts
@@ -1,10 +1,10 @@
-import { ComponentCustomProperties } from 'vue'
-import { Store } from 'vuex'
-import { State } from '@/store'
+import { ComponentCustomProperties } from "vue";
+import { Store } from "vuex";
+import { State } from "@/store";
 
-declare module '@vue/runtime-core' {
+declare module "@vue/runtime-core" {
   // this.$storeの型を宣言
   interface ComponentCustomProperties {
-    $store: Store<State>
+    $store: Store<State>;
   }
 }
